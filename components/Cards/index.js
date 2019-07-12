7bb1d5b3573b0edcc94d80d0c36ddd91a8007b1e
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-12 11:00:59 -0700

    in the middle of fixing the cards page
    making my 11am commit for sprint timer

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -21,47 +21,22 @@
 axios
   .get("https://lambda-times-backend.herokuapp.com/articles")
   .then(response => {
-    const placement = document.querySelector(".cards-container");
+    const javaScr = response.data.articles.javacript.map(funct => card(funct))
+    javaScr.forEach(funct => funct.setAttribute("data-subject", "javascript"))
 
-    for (Something of Object.values(response.data.articles)) {
-      Something.forEach(news => {
-        const card = cardMaker(news);
-        card.setAttribute("data-subject", "data");
-        placement.appendChild(card);
-      });
-    }
-  })
+    const bootSt = response.data.articles.bootstrap.map(funct => card(funct))
+    bootSt.forEach(funct => funct.setAttribute("data-subject", "bootstrap"))
 
-  .catch(e => {
+    const tech = response.data.articles.technology.map(funct => card(funct))
+    tech.forEach(funct => funct.setAttribute("data-subject", "technology"))
+
+    const jQuery = response.data.articles.technology.map(funct => card(funct));
+    jQuery.forEach(funct => funct.setAttribute("data-subject", "jQuery"))
+
+    const node = response.data.articles.technology.map(funct => card(funct));
+    node.forEach(funct => funct.setAttribute("data-subject", "node"))
+    
+    .catch(error => {
     console.log(
       "This site broke, just kidding -- it's a small error, but you don't blame me -- I am just a student!"
     );
-  });
-
-function cardMaker(sprint) {
-  const myCard = document.createElement("div");
-  myCard.classList.add("card");
-
-  const headline = document.createElement("div");
-  headline.classList.add("headline");
-  headline.textContent = sprint.headline;
-  myCard.appendChild(headline);
-
-  const author = document.createElement("div");
-  author.classList.add("author");
-  myCard.appendChild(author);
-
-  const img = document.createElement("div");
-  img.classList.add("img-container");
-  author.appendChild(img);
-
-  const actualImg = document.createElement("img");
-  actualImg.src = sprint.authorPhoto;
-  img.appendChild(actualImg);
-
-  const byline = document.createElement("span");
-  byline.textContent = sprint.authorName;
-  author.appendChild(byline);
-
-  return myCard;
-}
